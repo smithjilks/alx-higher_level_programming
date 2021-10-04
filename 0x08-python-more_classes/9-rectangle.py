b7ab1116f3d46ac6e68ac954532ef168b1ba44e6
smithjilks <smithjilks@gmail.com>
2021-10-04 20:38:07 +0300

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -25,7 +25,15 @@ class Rectangle:
         """
 
         Rectangle.number_of_instances += 1
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
