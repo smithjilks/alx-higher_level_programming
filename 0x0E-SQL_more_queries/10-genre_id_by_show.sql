1fa4ffb8fdc2649934c79355bdf276d6419de1f8
smithjilks <smithjilks@gmail.com>
2021-11-18 02:44:04 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,6 +6,6 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT shows.title, genre.genre_id  FROM tv_shows AS shows
+SELECT shows.title, genres.genre_id  FROM tv_shows AS shows
 INNER JOIN tv_show_genres AS genres ON shows.id = genres.show_id
 ORDER BY shows.title, genres.genre_id;
