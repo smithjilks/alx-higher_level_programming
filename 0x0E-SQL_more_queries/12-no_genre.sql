ae25f6659a5bc95989d8b4fcda10235c0834289c
smithjilks <smithjilks@gmail.com>
2021-11-18 02:58:59 +0300

    Add 12_no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,12 +1,11 @@
--- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked.
 
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title
    -- and tv_show_genres.genre_id
--- If a show doesn’t have a genre, display NULL
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
 SELECT shows.title, genres.genre_id FROM tv_shows AS shows
-LEFT JOIN tv_show_genres AS genres
-ON shows.id = genres.show_id  ORDER BY shows.title, genres.genre_id;
+LEFT JOIN tv_show_genres AS genres ON shows.id = genres.show_id
+WHERE genres.genre_id IS NULL ORDER BY shows.title, genres.genre_id;
