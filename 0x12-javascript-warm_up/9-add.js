776d3c4a5ba2a1037ec46e0cc8762b486b6189d0
smithjilks <smithjilks@gmail.com>
2021-12-14 00:31:19 +0300

    Add 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+prints the addition of 2 integers
+*/
+const number1 = parseInt(process.argv[2]);
+const number2 = parseInt(process.argv[3]);
+function add (a, b) {
+  return (a + b);
+}
+console.log(add(number1, number2));
