6f97236bd5aba5e1e2b634ea4578dc9841b783a2
smithjilks <smithjilks@gmail.com>
2021-12-14 00:56:02 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
 prints 3 lines: (like 1-multi_languages.js)
 but by using an array of string and a loop
 */
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+const strArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < strArray.length; i++) {
+  console.log(strArray[i]);
 }
