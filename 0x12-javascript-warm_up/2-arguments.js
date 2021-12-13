4b2bee78025f7d7d1b67d791f866da2eaaae79dd
smithjilks <smithjilks@gmail.com>
2021-12-13 23:51:23 +0300

    Add 2-arguements.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/bin/usr/node
+/*
+prints a message depending of the number of arguments passed
+*/
+
+
+const args = process.argv.length;
+if (args === 2) {
+  console.log('No argument');
+} else if (args === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
