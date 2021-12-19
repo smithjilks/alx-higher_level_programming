88f9135aaa5d4e26f61212855f5c42461c3af16d
smithjilks <smithjilks@gmail.com>
2021-12-19 14:56:04 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities"
-                "JOIN states ON cities.state_id = states.id"
-                "WHERE states.name LIKE %s"
+    cur.execute("SELECT cities.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "WHERE states.name LIKE %s "
                 "ORDER BY cities.id", (sys.argv[4],))
     query_rows = cur.fetchall()
 
