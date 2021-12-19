af79f1bf5929246cb6c22071ea5bf2bb7792d6f8
smithjilks <smithjilks@gmail.com>
2021-12-19 14:52:20 +0300

    Add 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""ists all cities from the database"""
+"""takes in the name of a state as an argument
+and lists all cities of that state"""
 
 if __name__ == "__main__":
     import MySQLdb
@@ -8,9 +9,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name "
-                "FROM cities JOIN states ON cities.state_id = states.id "
-                "ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.name FROM cities"
+                "JOIN states ON cities.state_id = states.id"
+                "WHERE states.name LIKE %s"
+                "ORDER BY cities.id", (sys.argv[4],))
     query_rows = cur.fetchall()
 
     for row in query_rows:
