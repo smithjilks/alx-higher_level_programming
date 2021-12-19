cf4beddd2d3c6dee06101371bd45b9dcfce68542
smithjilks <smithjilks@gmail.com>
2021-12-19 14:23:02 +0300

    Add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""filters states with a name starting with N (upper N) from the database"""
+"""takes in an argument and displays all values
+in the states table where name matches the argument."""
+
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -7,10 +9,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
     query_rows = cur.fetchall()
-
     for row in query_rows:
         print(row)
     cur.close()
