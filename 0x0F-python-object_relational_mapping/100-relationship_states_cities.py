f1ee5fd4ee4c03abc81c7fb3b1c8699fbeb16227
smithjilks <smithjilks@gmail.com>
2021-12-24 21:19:01 +0300

    Add 100-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+creates the State “California” with the City “San Francisco” from the database
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_State = State(name="California")
+    new_State.cities = [City(name="San Francisco")]
+    re = session.add(new_State)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ This class defines an object represtation of the table states
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True, unique=True,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
