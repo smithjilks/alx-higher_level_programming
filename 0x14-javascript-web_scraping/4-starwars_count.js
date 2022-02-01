9ab23ed11ccbbbdffa51a0eca4292b258797814f
smithjilks <smithjilks@gmail.com>
2022-02-02 00:19:08 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,6 +10,6 @@ request(url, (error, response, body) => {
     const movies = JSON.parse(body).results;
     const charArray = movies.map(movie => movie.characters)
       .filter(x => x.includes('https://swapi-api.hbtn.io/api/people/18/'));
-    console.log(charArray.length);
+    console.log(String(charArray.length));
   }
 });
