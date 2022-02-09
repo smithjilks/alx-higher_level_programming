c8e521692a2d3fddc0aa3fcd1326b937975be48b
smithjilks <smithjilks@gmail.com>
2022-02-09 22:45:54 +0300

    Add 9-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,9 @@
-$('DIV#toggle_header').click(function() {
-    if ($('header').attr('class') === 'red') {
-	$('header').removeAttr('class');
-	$('header').addClass('green')
-    } else if ($('header').attr('class') === 'green') {
-	$('header').removeAttr('class');
-	$('header').addClass('red');
-    }
+$('DIV#toggle_header').click(function () {
+  if ($('header').attr('class') === 'red') {
+    $('header').removeAttr('class');
+    $('header').addClass('green');
+  } else if ($('header').attr('class') === 'green') {
+    $('header').removeAttr('class');
+    $('header').addClass('red');
+  }
 });
